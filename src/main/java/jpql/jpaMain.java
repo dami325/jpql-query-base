c007a0fe9dadbcae35292ba03c81d9c2aa1b2831
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-18 19:40:07 +0900

    파라미터 바인딩 - 이름 기준, 위치 기준
    이름기준 : m.username = :username
    위치기준 : m.username = ?1
    이름기준 : .setParameter("username", "member1")
    위치기준 : .setParameter(1, "member1")

--- src/main/java/jpql/jpaMain.java
+++ src/main/java/jpql/jpaMain.java
@@ -23,17 +23,22 @@ public class jpaMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);     // 타입 정보가 명확할때
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)     // 타입 정보가 명확할때
+                    .setParameter("username", "member1")
+                    .getSingleResult();
 
-            Member result = query.getSingleResult();// 반환타입이 하나일때, 단 결과가 없다면 NoResultException 발생, 2개이상이면 NonUniqueResultException 발생
-            System.out.println("result = " + result);
-
-            List<Member> resultList = query.getResultList(); // 반환타입이 여러개일때 List
-
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
-//            Query query1 = em.createQuery("select m.username, m.age from Member m"); // 타입 정보가 명확하지 않을때
+            System.out.println("result = " + result.getUsername());
+//
+//            Member result = query.getSingleResult();// 반환타입이 하나일때, 단 결과가 없다면 NoResultException 발생, 2개이상이면 NonUniqueResultException 발생
+//            //Spring Data JPA -> 결과가 없어도 Exception 안터짐
+//            System.out.println("result = " + result);
+//
+//            List<Member> resultList = query.getResultList(); // 반환타입이 여러개일때 List
+//
+//            for (Member member1 : resultList) {
+//                System.out.println("member1 = " + member1);
+//            }
+////            Query query1 = em.createQuery("select m.username, m.age from Member m"); // 타입 정보가 명확하지 않을때
 
 
             tx.commit();
