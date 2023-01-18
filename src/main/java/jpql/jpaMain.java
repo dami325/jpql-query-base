ddd6c4d6823e992e7ff5a49fe7d88fd80910afda
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-19 00:50:21 +0900

    컬렉션 페치 조인과 DISTINCT(일대 다 페치 조인은 데이터가 뻥튀기됨)

--- src/main/java/jpql/jpaMain.java
+++ src/main/java/jpql/jpaMain.java
@@ -46,14 +46,16 @@ public class jpaMain {
             em.flush();
             em.clear();
 
-//            String query = "SELECT m.username from Member m"; 상태 필드 더이상 탐색 x
-//            String query = "SELECT m.team from Member m"; 단일 값 연관 경로 : 묵시적 내부 조인 발생(m.team.name) join쿼리 날라감
-            String query = "SELECT m from Member m join fetch m.team";// 컬렉션 값 연관 경로 : 묵시적 내부 조인 발생, 탐색x , from 절에서 명시적 조인을 통해 별칭을 얻으면 별칭을 통해 탐색 가능
+            String query = "select distinct t from Team t join fetch t.members";
 
-            List<Member> resultList = em.createQuery(query, Member.class)
+            List<Team> resultList = em.createQuery(query, Team.class)
                     .getResultList();
-            for (Member member : resultList) {
-                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+
+            for (Team team : resultList) {
+                System.out.println("team = " + team.getName() + " ,members= " + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("member = " + member);
+                }
             }
 
 
