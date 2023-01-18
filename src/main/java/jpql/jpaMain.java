b8b8c3034e4e979314d10acd76d6b15af65e56a9
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-18 21:40:12 +0900

    JPQL - 경로 표현식  - (.)점을 찍어 객체 그래프를 탐색하는 것
     상태필드 - 단순히 값을 저장하기 위한 필드(m.username)
     연관 필드 - 연관관계를 위한 필드
     1.단일 값 연관 필드 : @ManyToOne, @OneToOne, 대상이 엔티티(m.team)
     2.컬렉션 값 연관 필드: @OneToMany, @ManyToMany, 대상이 컬렉션(m.orders)

--- src/main/java/jpql/jpaMain.java
+++ src/main/java/jpql/jpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.Collection;
 import java.util.List;
 
 public class jpaMain {
@@ -31,14 +32,16 @@ public class jpaMain {
             em.flush();
             em.clear();
 
-            String query = "SELECT function('group_concat', m.username) from Member m";
-            List<String> result = em.createQuery(query, String.class)
+//            String query = "SELECT m.username from Member m"; 상태 필드 더이상 탐색 x
+//            String query = "SELECT m.team from Member m"; 단일 값 연관 경로 : 묵시적 내부 조인 발생(m.team.name) join쿼리 날라감
+            String query = "SELECT t.members from Team t";// 컬렉션 값 연관 경로 : 묵시적 내부 조인 발생, 탐색x , from 절에서 명시적 조인을 통해 별칭을 얻으면 별칭을 통해 탐색 가능
+
+            Collection result = em.createQuery(query, Collection.class)
                     .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
+            for (Object o : result) {
+                System.out.println("o = " + o);
             }
-
 
             tx.commit();
         } catch (Exception e) {
