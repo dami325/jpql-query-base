6352a70a6c68c924287b5312c40fda8a119bf871
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-18 21:21:22 +0900

    사용자 정의 함수 등록 및 호출

--- src/main/java/jpql/jpaMain.java
+++ src/main/java/jpql/jpaMain.java
@@ -18,24 +18,20 @@ public class jpaMain {
 
         try {
 
-                Team team = new Team();
-                team.setName("teamA");
-                em.persist(team);
 
-                Member member = new Member();
-                member.setUsername("관리자");
-                member.setAge(10);
-                member.setType(MemberType.ADMIN);
+                Member member1 = new Member();
+                member1.setUsername("관리자1");
+                em.persist(member1);
 
-
-                em.persist(member);
+                Member member2 = new Member();
+                member2.setUsername("관리자2");
+                em.persist(member2);
 
 
             em.flush();
             em.clear();
 
-            /** NULLIF : 두 값이 같으면 null 반환, 다르면 첫번째 값 반환*/
-            String query = "SELECT NULLIF(m.username,'관리자') as username from Member m";
+            String query = "SELECT function('group_concat', m.username) from Member m";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
