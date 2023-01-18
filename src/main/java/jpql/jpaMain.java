bf8babf34d10750531cf045cfe3d5cce27a427f1
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-18 19:31:51 +0900

    Initial commit

---
+++ src/main/java/jpql/jpaMain.java
@@ -0,0 +1,48 @@
+package jpql;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class jpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);     // 타입 정보가 명확할때
+
+            Member result = query.getSingleResult();// 반환타입이 하나일때, 단 결과가 없다면 NoResultException 발생, 2개이상이면 NonUniqueResultException 발생
+            System.out.println("result = " + result);
+
+            List<Member> resultList = query.getResultList(); // 반환타입이 여러개일때 List
+
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+//            Query query1 = em.createQuery("select m.username, m.age from Member m"); // 타입 정보가 명확하지 않을때
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
