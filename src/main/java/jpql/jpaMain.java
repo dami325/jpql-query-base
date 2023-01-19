588f66bfabeb04926bf6c872b06a723bc5e146ef
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-19 09:53:33 +0900

    벌크 연산 - update 후 영속성 컨텍스트 초기화한다음 다시 찾아와서 출력

--- src/main/java/jpql/jpaMain.java
+++ src/main/java/jpql/jpaMain.java
@@ -42,18 +42,21 @@ public class jpaMain {
                 member3.setTeam(teamB);
                 em.persist(member3);
 
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
 
-            em.flush();
             em.clear();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            Member findMember = em.find(Member.class, member1.getId());
 
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            System.out.println("findMember = " + findMember.getAge());
 
+            System.out.println("resultCount = " + resultCount);
+
+
+            System.out.println("member1.getAge() = " + member1.getAge()); // 0
+            System.out.println("member2.getAge() = " + member2.getAge()); // 0
+            System.out.println("member3.getAge() = " + member3.getAge()); // 0
 
             tx.commit();
         } catch (Exception e) {
